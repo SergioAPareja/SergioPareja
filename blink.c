9a8a79713f17f359f0b5a7c2f5ec043cdccd44dc
SergioAPareja <45397958+SergioAPareja@users.noreply.github.com>
2020-09-22 23:02:45 -0400

    Add files via upload

---
+++ blink.c
@@ -0,0 +1,308 @@
+/* Blink Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "driver/i2c.h"
+
+
+#define SLAVE_ADDR                         0x70 // alphanumeric address
+#define OSC                                0x21 // oscillator cmd
+#define HT16K33_BLINK_DISPLAYON            0x01 // Display on cmd
+#define HT16K33_BLINK_OFF                  0    // Blink off cmd
+#define HT16K33_BLINK_CMD                  0x80 // Blink cmd
+#define HT16K33_CMD_BRIGHTNESS             0xE0 // Brightness cmd
+
+// Master I2C
+#define I2C_EXAMPLE_MASTER_SCL_IO          22   // gpio number for i2c clk
+#define I2C_EXAMPLE_MASTER_SDA_IO          23   // gpio number for i2c data
+#define I2C_EXAMPLE_MASTER_NUM             I2C_NUM_0  // i2c port
+#define I2C_EXAMPLE_MASTER_TX_BUF_DISABLE  0    // i2c master no buffer needed
+#define I2C_EXAMPLE_MASTER_RX_BUF_DISABLE  0    // i2c master no buffer needed
+#define I2C_EXAMPLE_MASTER_FREQ_HZ         100000     // i2c master clock freq
+#define WRITE_BIT                          I2C_MASTER_WRITE // i2c master write
+#define READ_BIT                           I2C_MASTER_READ  // i2c master read
+#define ACK_CHECK_EN                       true // i2c master will check ack
+#define ACK_CHECK_DIS                      false// i2c master will not check ack
+#define ACK_VAL                            0x00 // i2c ack value
+#define NACK_VAL                           0xFF // i2c nack value
+
+/* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
+   or you can edit the following line and set a number here.
+*/
+#define servo 14
+#define button 21
+#define Bit1 27
+#define Bit2 33
+#define Bit3 15
+#define Bit4 32
+
+bool direction = true; //1 is up, 0 is down
+int state = 16;
+bool toggle[4] = {1,1,1,1};
+bool buttonPressed = false;
+
+
+static void LED(){
+    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
+       muxed to GPIO on reset already, but some default to other
+       functions and need to be switched to GPIO. Consult the
+       Technical Reference for a list of pads and their default
+       functions.)
+    */
+    gpio_pad_select_gpio(Bit1);
+    gpio_set_direction(Bit1, GPIO_MODE_OUTPUT);
+    gpio_pad_select_gpio(Bit2);
+    gpio_set_direction(Bit2, GPIO_MODE_OUTPUT);
+    gpio_pad_select_gpio(Bit3);
+    gpio_set_direction(Bit3, GPIO_MODE_OUTPUT);
+    gpio_pad_select_gpio(Bit4);
+    gpio_set_direction(Bit4, GPIO_MODE_OUTPUT);
+
+
+
+    while(1) {
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        gpio_set_level(Bit1, toggle[0]);
+        gpio_set_level(Bit2, toggle[1]);
+        gpio_set_level(Bit3, toggle[2]);
+        gpio_set_level(Bit4, toggle[3]);
+
+    if(state%2 == 0)
+    {
+        toggle[0] = !toggle[0];
+    }
+
+    if(state%4 == 0)
+        toggle[1] = !toggle[1];
+
+    if(state%8 == 0)
+        toggle[2] = !toggle[2];
+
+
+    if(state%16 == 0)
+        toggle[3] = !toggle[3];
+
+    printf("Direction is: %d\n", direction);
+
+    if(direction)
+        state= state + 1;
+    else
+        state = state -1;
+    if(state == 48 && direction == 1)
+        state = 16;
+    if(state == 16 && direction == 0)
+        state = 48;
+    }
+}
+
+static void ToggleUp()
+{
+    gpio_pad_select_gpio(button);
+    gpio_set_direction(button, GPIO_MODE_INPUT);
+	while(1){
+    if (gpio_get_level(button) == 1 && buttonPressed == false){
+      buttonPressed = true;
+      if (direction == 1) direction = 0;
+      else if (direction == 0) direction = 1;
+    }
+    else if (gpio_get_level(button) == 0){
+      buttonPressed = false;
+    }
+  vTaskDelay(50 / portTICK_PERIOD_MS);
+	}
+}
+
+
+
+static void i2c_example_master_init(){
+    // Debug
+    printf("\n>> i2c Config\n");
+    int err;
+
+    // Port configuration
+    int i2c_master_port = I2C_EXAMPLE_MASTER_NUM;
+
+    /// Define I2C configurations
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;                              // Master mode
+    conf.sda_io_num = I2C_EXAMPLE_MASTER_SDA_IO;              // Default SDA pin
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;                  // Internal pullup
+    conf.scl_io_num = I2C_EXAMPLE_MASTER_SCL_IO;              // Default SCL pin
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;                  // Internal pullup
+    conf.master.clk_speed = I2C_EXAMPLE_MASTER_FREQ_HZ;       // CLK frequency
+    err = i2c_param_config(i2c_master_port, &conf);           // Configure
+    if (err == ESP_OK) {printf("- parameters: ok\n");}
+
+    // Install I2C driver
+    err = i2c_driver_install(i2c_master_port, conf.mode,
+                       I2C_EXAMPLE_MASTER_RX_BUF_DISABLE,
+                       I2C_EXAMPLE_MASTER_TX_BUF_DISABLE, 0);
+    // i2c_set_data_mode(i2c_master_port,I2C_DATA_MODE_LSB_FIRST,I2C_DATA_MODE_LSB_FIRST);
+    if (err == ESP_OK) {printf("- initialized: yes\n\n");}
+
+    // Dat in MSB mode
+    i2c_set_data_mode(i2c_master_port, I2C_DATA_MODE_MSB_FIRST, I2C_DATA_MODE_MSB_FIRST);
+}
+
+// Utility  Functions //////////////////////////////////////////////////////////
+
+// Utility function to test for I2C device address -- not used in deploy
+int testConnection(uint8_t devAddr, int32_t timeout) {
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (devAddr << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    int err = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return err;
+}
+
+// Utility function to scan for i2c device
+static void i2c_scanner() {
+    int32_t scanTimeout = 1000;
+    printf("\n>> I2C scanning ..."  "\n");
+    uint8_t count = 0;
+    for (uint8_t i = 1; i < 127; i++) {
+        // printf("0x%X%s",i,"\n");
+        if (testConnection(i, scanTimeout) == ESP_OK) {
+            printf( "- Device found at address: 0x%X%s", i, "\n");
+            count++;
+        }
+    }
+    if (count == 0)
+        printf("- No I2C devices found!" "\n");
+    printf("\n");
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Alphanumeric Functions //////////////////////////////////////////////////////
+
+// Turn on oscillator for alpha display
+int alpha_oscillator() {
+  int ret;
+  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+  i2c_master_start(cmd);
+  i2c_master_write_byte(cmd, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+  i2c_master_write_byte(cmd, OSC, ACK_CHECK_EN);
+  i2c_master_stop(cmd);
+  ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
+  i2c_cmd_link_delete(cmd);
+  vTaskDelay(200 / portTICK_RATE_MS);
+  return ret;
+}
+
+// Set blink rate to off
+int no_blink() {
+  int ret;
+  i2c_cmd_handle_t cmd2 = i2c_cmd_link_create();
+  i2c_master_start(cmd2);
+  i2c_master_write_byte(cmd2, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+  i2c_master_write_byte(cmd2, HT16K33_BLINK_CMD | HT16K33_BLINK_DISPLAYON | (HT16K33_BLINK_OFF << 1), ACK_CHECK_EN);
+  i2c_master_stop(cmd2);
+  ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd2, 1000 / portTICK_RATE_MS);
+  i2c_cmd_link_delete(cmd2);
+  vTaskDelay(200 / portTICK_RATE_MS);
+  return ret;
+}
+
+// Set Brightness
+int set_brightness_max(uint8_t val) {
+  int ret;
+  i2c_cmd_handle_t cmd3 = i2c_cmd_link_create();
+  i2c_master_start(cmd3);
+  i2c_master_write_byte(cmd3, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+  i2c_master_write_byte(cmd3, HT16K33_CMD_BRIGHTNESS | val, ACK_CHECK_EN);
+  i2c_master_stop(cmd3);
+  ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd3, 1000 / portTICK_RATE_MS);
+  i2c_cmd_link_delete(cmd3);
+  vTaskDelay(200 / portTICK_RATE_MS);
+  return ret;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+static void test_alpha_display() {
+    // Debug
+    int ret;
+    printf(">> Test Alphanumeric Display: \n");
+
+    // Set up routines
+    // Turn on alpha oscillator
+    ret = alpha_oscillator();
+    if(ret == ESP_OK) {printf("- oscillator: ok \n");}
+    // Set display blink off
+    ret = no_blink();
+    if(ret == ESP_OK) {printf("- blink: off \n");}
+    ret = set_brightness_max(0xF);
+    if(ret == ESP_OK) {printf("- brightness: max \n");}
+
+    // Write to characters to buffer
+
+
+
+
+
+
+    uint16_t displaybuffer[8];
+
+
+
+    // Continually writes the same command
+    while (1) {
+
+
+    // Write to characters to buffer
+    if(direction){
+    displaybuffer[0] = 0b0000000000111110;  // U
+    displaybuffer[1] = 0b0000000011110011;  // P
+    displaybuffer[2] = 0b0000000000000000;
+    displaybuffer[3] = 0b0000000000000000;
+    }
+    else
+    {
+    displaybuffer[0] = 0b0001001000001111;  // D
+    displaybuffer[1] = 0b0000000000111111; // O
+    displaybuffer[2] = 0b0010100000110110, // W
+    displaybuffer[3] = 0b0010000100110110; // N
+    }
+
+      // Send commands characters to display over I2C
+      i2c_cmd_handle_t cmd4 = i2c_cmd_link_create();
+      i2c_master_start(cmd4);
+      i2c_master_write_byte(cmd4, ( SLAVE_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+      i2c_master_write_byte(cmd4, (uint8_t)0x00, ACK_CHECK_EN);
+      for (uint8_t i=0; i<8; i++) {
+        i2c_master_write_byte(cmd4, displaybuffer[i] & 0xFF, ACK_CHECK_EN);
+        i2c_master_write_byte(cmd4, displaybuffer[i] >> 8, ACK_CHECK_EN);
+      }
+      i2c_master_stop(cmd4);
+      ret = i2c_master_cmd_begin(I2C_EXAMPLE_MASTER_NUM, cmd4, 1000 / portTICK_RATE_MS);
+      i2c_cmd_link_delete(cmd4);
+
+      // for (int i = 0; i < 8; i++) {
+      //     printf("%04x\n", displaybuffer[i]);
+      // }
+
+    }
+    }
+
+
+void app_main(void)
+{
+    i2c_example_master_init();
+    i2c_scanner();
+    xTaskCreate(LED,"LED", 4096, NULL, configMAX_PRIORITIES-1, NULL);
+    xTaskCreate(ToggleUp,"ToggleUp", 4096, NULL, configMAX_PRIORITIES, NULL);
+    xTaskCreate(test_alpha_display,"test_alpha_display", 4096, NULL, configMAX_PRIORITIES-2, NULL);
+}
