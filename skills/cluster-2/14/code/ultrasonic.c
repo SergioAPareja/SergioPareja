ca18648f5f08e57a38b6f5c27e007c2147aa517c
SergioAPareja <45397958+SergioAPareja@users.noreply.github.com>
2020-10-12 21:42:06 -0400

    Add files via upload

---
+++ skills/cluster-2/14/code/ultrasonic.c
@@ -0,0 +1,127 @@
+/* ADC1 Example
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "driver/i2c.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+
+#define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
+#define NO_OF_SAMPLES   10          //Multisampling
+
+
+// 14-Segment Display
+#define SLAVE_ADDR                         0x70 // alphanumeric address
+#define OSC                                0x21 // oscillator cmd
+#define HT16K33_BLINK_DISPLAYON            0x01 // Display on cmd
+#define HT16K33_BLINK_OFF                  0    // Blink off cmd
+#define HT16K33_BLINK_CMD                  0x80 // Blink cmd
+#define HT16K33_CMD_BRIGHTNESS             0xE0 // Brightness cmd
+
+// Master I2C
+#define I2C_EXAMPLE_MASTER_SCL_IO          22   // gpio number for i2c clk
+#define I2C_EXAMPLE_MASTER_SDA_IO          23   // gpio number for i2c data
+#define I2C_EXAMPLE_MASTER_NUM             I2C_NUM_0  // i2c port
+#define I2C_EXAMPLE_MASTER_TX_BUF_DISABLE  0    // i2c master no buffer needed
+#define I2C_EXAMPLE_MASTER_RX_BUF_DISABLE  0    // i2c master no buffer needed
+#define I2C_EXAMPLE_MASTER_FREQ_HZ         100000     // i2c master clock freq
+#define WRITE_BIT                          I2C_MASTER_WRITE // i2c master write
+#define READ_BIT                           I2C_MASTER_READ  // i2c master read
+#define ACK_CHECK_EN                       true // i2c master will check ack
+#define ACK_CHECK_DIS                      false// i2c master will not check ack
+#define ACK_VAL                            0x00 // i2c ack value
+#define NACK_VAL                           0xFF // i2c nack value
+
+static esp_adc_cal_characteristics_t *adc_chars;
+static const adc_channel_t channel = ADC_CHANNEL_6;     //GPIO34 if ADC1, GPIO14 if ADC2
+static const adc_atten_t atten = ADC_ATTEN_DB_0;
+static const adc_unit_t unit = ADC_UNIT_1;
+
+//int DEFAULT_VREF = adc2_vref_to_gpio();
+
+static void check_efuse(void)
+{
+    //Check TP is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK) {
+        printf("eFuse Two Point: Supported\n");
+    } else {
+        printf("eFuse Two Point: NOT supported\n");
+    }
+
+    //Check Vref is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK) {
+        printf("eFuse Vref: Supported\n");
+    } else {
+        printf("eFuse Vref: NOT supported\n");
+    }
+}
+
+static void print_char_val_type(esp_adc_cal_value_t val_type)
+{
+    if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
+        printf("Characterized using Two Point Value\n");
+    } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
+        printf("Characterized using eFuse Vref\n");
+    } else {
+        printf("Characterized using Default Vref\n");
+    }
+}
+
+
+
+
+
+void app_main(void)
+{
+    //Check if Two Point or Vref are burned into eFuse
+
+    //Configure ADC
+    if (unit == ADC_UNIT_1) {
+        adc1_config_width(ADC_WIDTH_BIT_12);
+        adc1_config_channel_atten(channel, atten);
+    } else {
+        adc2_config_channel_atten((adc2_channel_t)channel, atten);
+    }
+
+    //Characterize ADC
+    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
+    print_char_val_type(val_type);
+
+    //Continuously sample ADC1
+    while (1) {
+        uint32_t adc_reading = 0;
+        //Multisampling
+        for (int i = 0; i < NO_OF_SAMPLES; i++) {
+            if (unit == ADC_UNIT_1) {
+                adc_reading += adc1_get_raw((adc1_channel_t)channel);
+            } else {
+                int raw;
+                adc2_get_raw((adc2_channel_t)channel, ADC_WIDTH_BIT_12, &raw);
+                adc_reading += raw;
+            }
+        }
+        adc_reading /= NO_OF_SAMPLES;
+        int count = 0;
+        int n = adc_reading;
+        //Convert adc_reading to voltage in mV
+        while (n) { 
+        count += n & 1; 
+        n >>= 1; 
+    }
+        int test = count *5;
+        //uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
+        printf("Raw: %d", test);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        //mid 90s seems to be around 30 cm
+        //test_alpha_display(voltage);
+    }
+}
